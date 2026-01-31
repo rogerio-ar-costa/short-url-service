938d18a9cb82defd36cecab75e5015aba569c3d9
rogerio-ar-costa <rogerio.ar.costa@gmail.com>
2026-01-31 01:20:16 +0000

    Improve integration tests

--- src/test/groovy/io/rac/shortener/controller/RedirectControllerIntegrationTest.groovy
+++ src/test/groovy/io/rac/shortener/controller/RedirectControllerIntegrationTest.groovy
@@ -1,6 +1,8 @@
 package io.rac.shortener.controller
 
 import io.rac.shortener.AbstractIntegrationTestSpecification
+import io.rac.shortener.dto.ShortenUrlRequest
+import io.rac.shortener.dto.ShortenUrlResponse
 import org.springframework.http.HttpStatus
 import org.springframework.http.client.SimpleClientHttpRequestFactory
 import org.springframework.web.client.RestTemplate
@@ -10,14 +12,11 @@ class RedirectControllerIntegrationTest extends AbstractIntegrationTestSpecifica
     def "redirect: happy path -> success"() {
         given:
         def originalUrl = "https://www.google.com"
-        def createResponse = restTemplate.postForEntity(getBaseUrl() + "/api/v1/urls", originalUrl, String.class)
-        def shortCode = createResponse.body
+        def request = new ShortenUrlRequest(originalUrl: originalUrl)
+        def createResponse = restTemplate.postForEntity(getBaseUrl() + "/api/v1/urls", request, ShortenUrlResponse.class)
+        def shortCode = createResponse.body.shortCode
 
         // Configure RestTemplate to NOT follow redirects so we can verify the 302
-        // We use a custom interceptor or a request factory.
-        // SimpleClientHttpRequestFactory does not have setOutputStreaming(boolean) in some versions,
-        // but it SHOULD have it. Let's try without it or use another way.
-        // Actually, let's just use a custom RequestFactory that overrides prepareConnection
         def requestFactory = new SimpleClientHttpRequestFactory() {
             @Override
             protected void prepareConnection(HttpURLConnection connection, String httpMethod) throws IOException {
@@ -28,7 +27,7 @@ class RedirectControllerIntegrationTest extends AbstractIntegrationTestSpecifica
         def noRedirectRestTemplate = new RestTemplate(requestFactory)
 
         when:
-        def response = noRedirectRestTemplate.getForEntity(getBaseUrl() + "/" + shortCode, String.class)
+        def response = noRedirectRestTemplate.getForEntity(getBaseUrl() + "/redirect-me/" + shortCode, Void.class)
 
         then:
         response.statusCode == HttpStatus.FOUND
--- src/test/groovy/io/rac/shortener/controller/UrlShortenerControllerIntegrationTest.groovy
+++ src/test/groovy/io/rac/shortener/controller/UrlShortenerControllerIntegrationTest.groovy
@@ -1,50 +1,92 @@
 package io.rac.shortener.controller
 
 import io.rac.shortener.AbstractIntegrationTestSpecification
+import io.rac.shortener.dto.ErrorResponse
+import io.rac.shortener.dto.ShortenUrlRequest
+import io.rac.shortener.dto.ShortenUrlResponse
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.web.client.HttpClientErrorException
-import org.springframework.web.client.RestTemplate
 
-class cleUrlShortenerControllerIntegrationTest extends AbstractIntegrationTestSpecification {
+class UrlShortenerControllerIntegrationTest extends AbstractIntegrationTestSpecification {
 
     def "create short url: happy path -> created"() {
         given:
         def originalUrl = "https://www.google.com"
+        def request = new ShortenUrlRequest(originalUrl: originalUrl)
 
         when:
-        def response = restTemplate.postForEntity(getBaseUrl() + "/api/v1/urls", originalUrl, String.class)
+        def response = restTemplate.postForEntity(
+                getBaseUrl() + "/api/v1/urls",
+                request,
+                ShortenUrlResponse.class
+        )
 
         then:
         response.statusCode == HttpStatus.CREATED
         response.body != null
+        response.body.shortCode != null
+        response.body.originalUrl == originalUrl
         response.headers.getLocation() != null
+    }
+
+    def "create short url: invalid url -> bad request"() {
+        given:
+        def invalidUrl = "invalid-url"
+        def request = new ShortenUrlRequest(originalUrl: invalidUrl)
+
+        when:
+        restTemplate.postForEntity(
+                getBaseUrl() + "/api/v1/urls",
+                request,
+                ShortenUrlResponse.class
+        )
+        
+        then:
+        def response = thrown(HttpClientErrorException.BadRequest)
+        response.statusCode == HttpStatus.BAD_REQUEST
+
+        and:
+        def errorBody = response.getResponseBodyAs(ErrorResponse.class)
+        errorBody.status == 400
+        errorBody.error == "Bad Request"
+        errorBody.message.contains("Invalid URL format")
     }
 
     def "get original url: happy path -> success"() {
         given:
         def originalUrl = "https://www.google.com"
-        def createResponse = restTemplate.postForEntity(getBaseUrl() + "/api/v1/urls", originalUrl, String.class)
-        def shortCode = createResponse.body
+        def request = new ShortenUrlRequest(originalUrl: originalUrl)
+        def createResponse = restTemplate.postForEntity(
+                getBaseUrl() + "/api/v1/urls",
+                request,
+                ShortenUrlResponse.class
+        )
+        def shortCode = createResponse.body.shortCode
 
         when:
-        def response = restTemplate.getForEntity(getBaseUrl() + "/api/v1/urls/" + shortCode, String.class)
+        def response = restTemplate.getForEntity(
+                getBaseUrl() + "/api/v1/urls/" + shortCode,
+                ShortenUrlResponse.class
+        )
 
         then:
         response.statusCode == HttpStatus.OK
-        response.body == originalUrl
+        response.body.originalUrl == originalUrl
+        response.body.shortCode == shortCode
     }
 
     def "get original url: unknown code -> not found"() {
         when:
-        def response
-        try {
-            response = restTemplate.getForEntity(getBaseUrl() + "/api/v1/urls/unknownCode", String.class)
-        } catch (HttpClientErrorException.NotFound e) {
-            response = new ResponseEntity<>(e.getStatusCode())
-        }
+        restTemplate.getForEntity(getBaseUrl() + "/api/v1/urls/unknownCode", ErrorResponse.class)
 
         then:
+        def response = thrown(HttpClientErrorException.NotFound)
         response.statusCode == HttpStatus.NOT_FOUND
+
+        and:
+        def errorBody = response.getResponseBodyAs(ErrorResponse.class)
+        errorBody.status == 404
+        errorBody.error == "Not Found"
+        errorBody.message == "URL not found for code: unknownCode"
     }
 }
